753823a32f461996cd5f9af1da30f0708039d221
DimaZ <dimazubkov99@gmail.com>
2021-09-09 23:04:00 +0300

    finished main

--- #src/js/main.js
+++ #src/js/main.js
@@ -1,8 +1,26 @@
-// const mainConteiners = document.querySelector('.main__conteiners');
-// if(window.innerWidth < 1100) {
-//     mainConteiners.style.display = 'block'
-// } else {
-//     mainConteiners.style.display = 'grid'
-// }
+const spanItems = document.querySelectorAll('.main__conteiners-item--text span');
+const conteiner = document.querySelectorAll('.main__conteiners-item');
+
+spanItems.forEach(item => {
+    item.addEventListener('click', (event) => {
+        
+        const target = event.target;
+        const previousText = target.previousSibling.previousSibling;
+        previousText.classList.toggle('hide');
+        item.innerHTML = 'Свернуть ↑'
+
+        if(!previousText.classList.contains('hide')) {
+            item.innerHTML = 'Свернуть ↑'
+        } else {
+        item.innerHTML = 'Развернуть ↓'
+        }
+        
+    })
+});
 
 
+
+
+if(window.innerWidth < 900) {
+
+}
